6f0bc3d48dcf5e8c916e8bfe3779dcab45dfda9f
Azizbek Topilboyev <azizbektopilboyev7@gmail.com>
2026-02-17 03:33:53 +0500

    added logic code

--- guesses.py
+++ guesses.py
@@ -5,9 +5,10 @@ guess = int(input( "guess a number between 0 and 10: "))
             
 guesses = 0
 while True:
-	if n == guess:
-				print("Topdingingiz!")
-				break
-	if guesses >= 3:
-				print("Yutqazdingiz!")
-				break
+    if n==guess:
+        print("toping")
+        break
+    
+    if guesses>=3:
+        print('yutqazding')
+        break
